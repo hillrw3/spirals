3fe8754575196420a52c4c5e6ab4535617158dd3
Rob Hill <robert.hill0122@gmail.com>
2014-10-08 10:14:09 -0600

    Set up and making progress on solving

---
+++ lib/spiral.rb
@@ -0,0 +1,34 @@
+def spiral(starting_number, size)
+  matrix = Array.new(size) { Array.new(size) }
+  path = [*starting_number...(size**2 + starting_number)]
+  matrix[(size/2)][(size/2)] = path.pop
+  matrix[(size/2)][(size/2 -1)] = path.pop
+  matrix[0] = path.shift(size)
+  matrix
+  path
+end
+
+p spiral(1, 3)
+
+
+#FOUND ONLINE
+def spiral_matrix(n)
+  matrix = Array.new(n) { Array.new(n) }
+  path = [*1..n*n]
+  padding = (n*n).to_s.size
+  layer = 0
+  until path.empty?
+    matrix[layer].map! { |l| l || path.shift }
+    matrix = matrix.transpose.reverse
+    layer += 1 unless matrix[layer].include?(nil)
+  end
+  matrix = matrix.transpose.reverse until matrix[0][0] == 1
+  matrix.transpose.each do |row|
+    row.each do |l|
+      print "%0#{padding}d" % l, ' '
+    end
+    puts
+  end
+end
+
+# spiral_matrix(2)
---
+++ spec/spirals_spec.rb
@@ -0,0 +1,9 @@
+#
+#
+# describe  "sprials" do
+#
+#   it "returns a grid with numbers in a spiral" do
+#     expect(Spiral.new(1,2)).to eq()
+#   end
+#
+# end
